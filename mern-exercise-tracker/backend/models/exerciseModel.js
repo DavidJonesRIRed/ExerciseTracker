1ef35297a6968edcfc220f2c738a03af181cfbdf
David Jones <coreldavid@hotmail.com>
2021-03-15 03:29:46 -0400

    Routes created

--- mern-exercise-tracker/backend/models/exerciseModel.js
+++ mern-exercise-tracker/backend/models/exerciseModel.js
@@ -5,12 +5,12 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema({
     userName: { type: String, required: true },
     description: { type: String, required: true },
-    duration: { type: Number, required, true },
+    duration: { type: Number, required: true },
     date: { type: Date, required: true },
 },{
     timestamps: true,
 });
 
-const Excerise = mongoose.model('Exercise', exerciseSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
 module.exports = Exercise;
